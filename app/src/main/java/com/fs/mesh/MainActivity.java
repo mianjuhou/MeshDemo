6c29d8f2f3e073d3a81d4713a1203e812211f865
mianjuhou <mianjuhou@163.com>
2016-05-09 16:05:29 +0800

    吸入式动画

--- app/src/main/java/com/fs/mesh/MainActivity.java
+++ app/src/main/java/com/fs/mesh/MainActivity.java
@@ -3,21 +3,28 @@ package com.fs.mesh;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.fs.mesh.widget.CurveView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-    private CurveView mCv;
-    private ImageView mIv;
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.btn)
+    Button mBtn;
+    @Bind(R.id.cv)
+    CurveView mCv;
+    @Bind(R.id.iv)
+    ImageView mIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mIv = (ImageView) findViewById(R.id.iv);
-        mCv = (CurveView) findViewById(R.id.cv);
+        ButterKnife.bind(this);
+
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,7 +37,5 @@ public class MainActivity extends AppCompatActivity {
                 mIv.setImageResource(R.drawable.ljt_have);
             }
         });
-
-
     }
 }
